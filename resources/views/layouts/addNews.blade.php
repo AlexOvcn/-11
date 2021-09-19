ac9601545c014ae324dae3273d32e86cb1cf868c
AlexOvcn <78850938+AlexOvcn@users.noreply.github.com>
2021-09-19 07:04:05 +0300

    Мелкое исправление

--- resources/views/layouts/addNews.blade.php
+++ resources/views/layouts/addNews.blade.php
@@ -8,7 +8,7 @@
             @csrf
             <label class="addNewsForm-label"><span class="addNewsForm-label__title">Header: </span><input name="summary" type="text" class="addNewsForm-label__input" oninput="inputRestriction(50, this)" required pattern=".{2,}"></label>
             <label class="addNewsForm-label"><span class="addNewsForm-label__title">Short text: </span><input name="short_description" type="text" class="addNewsForm-label__input" oninput="inputRestriction(150, this)"  required pattern=".{2,}"></label>
-            <label class="addNewsForm-label"><span class="addNewsForm-label__title">Article: </span><textarea name="full_text"  rows="20" class="addNewsForm-label__input" oninput="inputRestriction(5000, this)"  required pattern=".{2,}"></textarea></label>
+            <label class="addNewsForm-label"><span class="addNewsForm-label__title">Article: </span><textarea name="full_text"  rows="20" class="addNewsForm-label__input" oninput="inputRestriction(5000, this)"  required minlength='2'></textarea></label>
             <button class="addNewsForm-btn" type="submit">Add</button>
         </form>
     </div>
